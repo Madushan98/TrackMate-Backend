1d261569a14e5000257ee748d29f3668a689d39b
madushan <ranasinhemadushan@gmail.com>
2022-07-06 20:28:28 +0530

    creat lambda entryPoint

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,48 +1,14 @@
-using AuthService.Mapper;
-using AuthService.Services;
-using AutoMapper;
-using Base;
-using BaseService.DataContext;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-var connectionString = EnvConstants.DbConnection!;
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
-builder.Services.AddDbContext<DBContext>(
-    dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion,
-            builder =>
-                builder.MigrationsAssembly(
-                    "BaseService"))
-        .EnableDetailedErrors());
-var config = new MapperConfiguration(cfg => { cfg.AddProfile(new UserProfile()); });
-
-var mapper = config.CreateMapper();
-
-builder.Services.AddSingleton(mapper);
-
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace AuthService
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();

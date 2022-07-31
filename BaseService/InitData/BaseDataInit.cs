57621d7f59963f73a3d60b7bb9b319197ec80805
madushan <ranasinhemadushan@gmail.com>
2022-07-31 11:29:21 +0530

    feat(initData): user data init

---
+++ BaseService/InitData/BaseDataInit.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Base.InitData;
+
+public abstract class BaseDataInit<T> where T : class
+{
+    public virtual void Init(ModelBuilder modelBuilder)
+    {
+        var trackEntities = Data();
+        if (trackEntities == null) return;
+       
+
+        modelBuilder.Entity<T>().HasData(trackEntities);
+    }
+    public abstract List<T> Data();
+}

e0e14c4524c3a8b5721452ea75ff5ec4e49630de
madushan <ranasinhemadushan@gmail.com>
2022-08-12 11:24:20 +0530

    feat(Pass): Pass lambda entry point created

---
+++ PassService/LambdaEntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using AdminService;
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PassService
+
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}

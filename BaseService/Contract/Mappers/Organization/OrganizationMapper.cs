6bc48c6e15f7d5a08ab7d55bd1e84f1e95118343
madushan <ranasinhemadushan@gmail.com>
2022-12-18 13:43:12 +0530

    feat(Organization): Organization Data transfer objects Created

---
+++ BaseService/Contract/Mappers/Organization/OrganizationMapper.cs
@@ -0,0 +1,21 @@
+﻿using AuthService.Domain.Filters;
+using AuthService.Models.Request.Queries;
+using AutoMapper;
+using DAOLibrary.Organization;
+using DAOLibrary.User;
+using DTOLibrary.Common;
+using DTOLibrary.OrganizationDto;
+using DTOLibrary.UserDto;
+
+namespace BaseService.Contract.Mappers.Organization;
+
+public class OrganizationProfile : Profile
+{
+    public OrganizationProfile() 
+    {
+        CreateMap<OrganizationDao, CreateOrganizationRequest>().ReverseMap();
+        CreateMap<OrganizationDao, OrganizationResponse>().ReverseMap();
+        CreateMap<PaginationRequest, PaginationFilter>().ReverseMap();
+  
+    }
+}
---
+++ DTOLibrary/OrganizationDto/OrganizationResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOLibrary.OrganizationDto;
+
+public class OrganizationResponse
+{
+    public Guid Id { get; set; }
+
+    public string OrganizationName { get; set; }
+
+    public string OrganizationType { get; set; }
+
+    public bool IsApproved { get; set; }
+}

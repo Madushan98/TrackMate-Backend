cb0207c1d4dbccacccd8cc6d33b20158c542e7fa
madushan <ranasinhemadushan@gmail.com>
2022-12-18 14:45:23 +0530

    feat(organization):  lambda integration added

---
+++ OrganizationService/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace OrganizationService
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}

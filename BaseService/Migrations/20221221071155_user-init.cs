7a81522147f114f59fc5b2d91c78590e9620f219
madushan <ranasinhemadushan@gmail.com>
2022-12-21 13:14:44 +0530

    feat(Base): migration files added

---
+++ BaseService/Migrations/20221221071155_user-init.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseService.Migrations
+{
+    public partial class userinit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "Age", "DeviceId", "District", "EmergencyContactNumber", "FullName", "Gender", "IsVertified", "Iv", "JoinedDate", "Key", "Location", "NationalId", "OrganizationId", "Password", "PrimaryContactNumber", "Town", "UserType" },
+                values: new object[] { new Guid("11111111-1111-1111-1111-121111112112"), null, null, null, null, null, "Chathura Nuwan", null, false, "Qn0j+NTJFfCQ2Hw6WdjXqA==", null, "S6yeuQw4VMYmzvTapS/Jph3oUct88Iqq0XprXWxfMpQ=", null, "9812345678", null, "KcsI7gpDcpIWTwuCQUIvDA==", null, null, "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-121111112112"));
+        }
+    }
+}

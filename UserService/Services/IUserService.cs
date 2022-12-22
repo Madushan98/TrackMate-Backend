4c67070fbbe6ef2a3ec7b48435203ecbfe88fae4
madushan <ranasinhemadushan@gmail.com>
2022-12-22 23:59:01 +0530

    feat(User): getUser details api created

--- UserService/Services/IUserService.cs
+++ UserService/Services/IUserService.cs
@@ -1,8 +1,11 @@
-﻿using DTOLibrary.UserDto.AddOrganization;
+﻿using DTOLibrary.UserDto;
+using DTOLibrary.UserDto.AddOrganization;
 
 namespace UserService.Services;
 
 public interface IUserService
 {
+    Task<UserResponse> GetUserDetailsAsync(Guid userId);
+
     Task<UpdateUserOrganizationResponse> UpdateUserOrganization(UpdateUserOrganizationRequest request);
 }
--- UserService/Services/UserService.cs
+++ UserService/Services/UserService.cs
@@ -2,7 +2,9 @@
 using BaseService.Constants;
 using BaseService.DataContext;
 using DAOLibrary.Organization;
+using DAOLibrary.User;
 using DTOLibrary.OrganizationDto;
+using DTOLibrary.UserDto;
 using DTOLibrary.UserDto.AddOrganization;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,7 +20,18 @@ public class UserService : IUserService
         _context = context;
         _mapper = mapper;
     }
-    
+
+    public  async Task<UserResponse> GetUserDetailsAsync(Guid userId)
+    {
+        var user  = await  _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Id == userId);
+        
+        var response = _mapper.Map<UserResponse>(user);
+        
+        return response;
+    }
+
     public async Task<UpdateUserOrganizationResponse> UpdateUserOrganization(UpdateUserOrganizationRequest request)
     {
         var user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Id == request.UserId);
@@ -35,9 +48,8 @@ public class UserService : IUserService
         _context.Users.Update(user);
         await _context.SaveChangesAsync();
         return new UpdateUserOrganizationResponse()
-        { 
+        {
             Message = "Organization Details Is submitted",
         };
-  
     }
 }

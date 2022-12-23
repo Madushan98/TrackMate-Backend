618723f7c86dd38cd3390f2d70c6f1b983391abd
madushan <ranasinhemadushan@gmail.com>
2022-12-23 05:48:11 +0530

    feat: add migration files

---
+++ BaseService/Migrations/20221222223657_passLogUserNatId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseService.Migrations
+{
+    public partial class passLogUserNatId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserNatId",
+                table: "PassLogs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserNatId",
+                table: "PassLogs");
+        }
+    }
+}
---
+++ BaseService/Migrations/20221223000758_passlogEncrypt.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseService.Migrations
+{
+    public partial class passlogEncrypt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PassLogEncrypts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Latitude = table.Column<string>(type: "text", nullable: false),
+                    Longitude = table.Column<string>(type: "text", nullable: false),
+                    LogTime = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: false),
+                    ScannerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserNatId = table.Column<string>(type: "text", nullable: false),
+                    PassId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassLogEncrypts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PassLogEncrypts_Passes_PassId",
+                        column: x => x.PassId,
+                        principalTable: "Passes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PassLogEncrypts_Users_ScannerId",
+                        column: x => x.ScannerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PassLogEncrypts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassLogEncrypts_PassId",
+                table: "PassLogEncrypts",
+                column: "PassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassLogEncrypts_ScannerId",
+                table: "PassLogEncrypts",
+                column: "ScannerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassLogEncrypts_UserId",
+                table: "PassLogEncrypts",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassLogEncrypts");
+        }
+    }
+}

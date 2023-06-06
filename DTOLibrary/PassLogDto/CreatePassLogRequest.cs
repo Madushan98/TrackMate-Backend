ae16fff1bbadcb329777546a01c9be40b0e8819b
madushan <ranasinhemadushan@gmail.com>
2023-06-06 12:39:13 +0530

    Pass model changed

--- DTOLibrary/PassLogDto/CreatePassLogRequest.cs
+++ DTOLibrary/PassLogDto/CreatePassLogRequest.cs
@@ -4,12 +4,7 @@ public class CreatePassLogRequest
 {
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
-    
-    
-    public string Date { get; set; }
     public Guid ScannerId { get; set; }
-    
     public Guid PassId { get; set; }
-    
     public Guid UserId { get; set; }
 }
